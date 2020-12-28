c5e26fdd77f861b241cf9c2377a0ce72b9cdec1e
Shlomo666 <shlomo366@gmail.com>
2020-12-28 10:50:51 +0200

    Add option to change shortcut.

--- main/actions.js
+++ main/actions.js
@@ -1,19 +1,42 @@
 const { globalShortcut, screen } = require('electron');
-const { getCursorScreenPoint, getDisplayNearestPoint } = screen;
+const path = require('path');
+const { readFile, writeFileSync, readFileSync } = require('fs');
 
-const shortcut = 'alt+space';
+const { getCursorScreenPoint, getDisplayNearestPoint } = screen;
+const appDir = path.dirname(require.main.filename);
+
+const shortcutFilePath = `${appDir}/main/store/shortcut.txt`;
+
+let shortcut = 'alt+space';
+readFile(shortcutFilePath, (err, buf) => exports.setShortcut(buf.toString()));
+exports.getShortcut = () => {
+  return readFileSync(shortcutFilePath).toString();
+};
+exports.setShortcut = (newShortcut) => {
+  if (registeredCallback) {
+    globalShortcut.unregister(shortcut);
+    globalShortcut.register(newShortcut, registeredCallback);
+  }
+  writeFileSync(shortcutFilePath, newShortcut);
+  shortcut = newShortcut;
+};
+
+let registeredCallback;
 
 /** @param {Electron.BrowserWindow} win */
 exports.hide = (win) => {
   win.hide();
-  globalShortcut.register(shortcut, () => {
+
+  registeredCallback = () => {
     exports.show(win);
-  });
+  };
+  globalShortcut.register(shortcut, registeredCallback);
 };
 
 /** @param {Electron.BrowserWindow} win */
 exports.show = (win) => {
   globalShortcut.unregister(shortcut);
+  registeredCallback = null;
 
   const currentScreen = getDisplayNearestPoint(getCursorScreenPoint());
   const { x, y } = currentScreen.workArea;
--- main/tray.js
+++ main/tray.js
@@ -1,5 +1,6 @@
 const { Tray, screen, Menu, app } = require('electron');
 const path = require('path');
+const { setShortcut, getShortcut } = require('./actions');
 
 const appDir = path.dirname(require.main.filename);
 const package = require(appDir + '/package.json');
@@ -11,12 +12,17 @@ exports.setTray = () => {
     tray = new Tray(appDir + '/menu_bar_icon.png');
   }
   tray.setToolTip(package.productName);
+  const shortcut = getShortcut();
   tray.setContextMenu(
     Menu.buildFromTemplate([
       {
+        label: 'Shortcut',
+        submenu: Menu.buildFromTemplate(['alt+space', 'ctrl+space'].map((key) => ({ label: key, click: () => setShortcut(key), type: 'radio', checked: key === shortcut }))),
+      },
+      {
         label: 'Quit',
-        click: () => app.quit()
-      }
+        click: () => app.quit(),
+      },
     ])
   );
 };
