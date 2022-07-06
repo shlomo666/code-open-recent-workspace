7ff83ce7b74de5fe1abceb0026bb623ce08fa5ed
shlomo-artlist <82378873+shlomo-artlist@users.noreply.github.com>
2022-07-06 11:49:12 +0300

    Fixes due to new vscode version - backwards compatible

--- preload/vscodeSettingsReader.js
+++ preload/vscodeSettingsReader.js
@@ -6,11 +6,18 @@ const vscodeWorkspaceStoragePath = `${process.env.HOME}/Library/Application Supp
 const folderPathPrefix = 'file://';
 
 const getListOfWorkspacesByRecentOrder = () => {
-  const workspaceIds = fs.readdirSync(vscodeWorkspaceStoragePath);
+  console.log('getListOfWorkspacesByRecentOrder...');
+  const workspaceIds = fs
+    .readdirSync(vscodeWorkspaceStoragePath, { withFileTypes: true })
+    .filter((f) => f.isDirectory())
+    .map((f) => f.name)
+    .filter((id) => fs.existsSync(`${vscodeWorkspaceStoragePath}/${id}/workspace.json`));
+
   /** @type {string[]} */
   const folderUris = workspaceIds.map((id) => require(`${vscodeWorkspaceStoragePath}/${id}/workspace.json`).folder);
   const paths = folderUris.filter((f) => f && f.startsWith(folderPathPrefix)).map((p) => decodeURI(p.slice(folderPathPrefix.length)));
   const existingPaths = paths.filter((path) => fs.existsSync(path));
+  console.log({ existingPaths });
   return existingPaths;
 };
 
